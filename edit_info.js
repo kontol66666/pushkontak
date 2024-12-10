1d07b5826728ec5cd659e4a429ba9051a3a5d56c
kontol66666 <akagaminojeki39@gmail.com>
2024-12-10 21:12:01 +0700

    Add files via upload

---
+++ edit_info.js
@@ -0,0 +1,25 @@
+const { modul } = require('./module');
+
+global.urlpepe = `https://deposit.pictures/p/1ea131b695b448c4aac785ae2072357f`// url/link foto
+
+global.namaowner = `SANNZSTORE`// nama owner
+global.noowner = `6285771667389`// nomor owner
+
+global.packname = `ayam`// nama pack sticker
+global.author = `kokok`// nama author stiker
+
+global.pending = `1000`// waktu tunggu broadcast
+/**EDIT WAKTU TUNGGU BROADCAST DI ATAS
+CONTOH :
+1000 = 1 DETIK
+2000 = 2 DETIK
+3000 = 3 DETIK
+DAN SETERUS NYA
+
+🚫TIDAK DI SARAN KAN UNTUK DI BAWAH 1 DETIK🚫
+KARENA AKAN MENYEBAB KAN WA TERBLOKIR
+
+BELI SCRIPT NO ENC, CHAT WHATSAPP DI BAWAH
+HARGA : Rp 50.000
+NOMOR WA : 6285771667389
+**/
