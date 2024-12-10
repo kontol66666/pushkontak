d10c0f27975eae734eed657deb36ad7a423d68a5
kontol66666 <akagaminojeki39@gmail.com>
2024-12-10 21:14:40 +0700

    Update edit_info.js

--- edit_info.js
+++ edit_info.js
@@ -3,12 +3,12 @@ const { modul } = require('./module');
 global.urlpepe = `https://deposit.pictures/p/1ea131b695b448c4aac785ae2072357f`// url/link foto
 
 global.namaowner = `SANNZSTORE`// nama owner
-global.noowner = `6285771667389`// nomor owner
+global.noowner = `6285213570081`// nomor owner
 
 global.packname = `ayam`// nama pack sticker
 global.author = `kokok`// nama author stiker
 
-global.pending = `1000`// waktu tunggu broadcast
+global.pending = `3000`// waktu tunggu broadcast
 /**EDIT WAKTU TUNGGU BROADCAST DI ATAS
 CONTOH :
 1000 = 1 DETIK
